b1ca27728549bd4815bd1856b57f767a65975b50
Josh Vigar <joshxx2@btinternet.com>
2020-08-14 14:47:47 +0100

    Single job page scraping implemented

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source "https://rubygems.org"
 gem 'httparty'
 gem 'nokogiri'
 gem 'byebug'
+
+group :test, :development do
+  gem 'prettier'
+  gem 'solargraph'
+end
--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,37 @@
-require 'nokogiri'
-require 'httparty'
-require 'byebug'
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
 
 def scraper
-    url = "https://blockchain.work.cc/"
-    unparsed_page = 
+  url = 'https://www.indeed.co.uk/software-developer-jobs-in-Birmingham'
+  unparsed_page = HTTParty.get(url)
+  if unparsed_page.body.nil? || unparsed_page.body.empty?
+    puts 'GET did not return a page'
+    exit
+  end
+
+  parsed_page = Nokogiri.HTML(unparsed_page)
+  jobs = Array.new
+  job_listings = parsed_page.css('div.jobsearch-SerpJobCard')
+  per_page = job_listings.count
+  puts total =
+         parsed_page.css(
+           'div.resultsTop > div.secondRow > div > div > div#searchCountPages'
+         ).text.strip.split[
+           3
+         ]
+  job_listings.each do |job_listing|
+    job = {
+      title: job_listing.css('a.jobtitle').text,
+      company: job_listing.css('span.comapany'),
+      location: job_listing.css('div.location').text,
+      url:
+        'https://www.indeed.co.uk' +
+          job_listing.css('a')[0].attributes['href'].value
+    }
+    jobs << job
+  end
+  byebug
 end
+
+scraper
