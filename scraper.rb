90fe6b1208bba65d706ddbee90dea1bb2877ed97
Josh Vigar <joshxx2@btinternet.com>
2020-08-16 00:11:40 +0100

    added calculation for total page count for search

--- scraper.rb
+++ scraper.rb
@@ -2,9 +2,36 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
-def scraper
-  url = 'https://www.indeed.co.uk/software-developer-jobs-in-Birmingham'
-  unparsed_page = HTTParty.get(url)
+URL = 'https://www.indeed.co.uk/jobs?q=software+developer&start='
+
+def getTotal
+  curUrl = URL + "10"
+  unparsed_page = HTTParty.get(curUrl)
+  
+  if unparsed_page.body.nil? || unparsed_page.body.empty?
+    puts 'GET did not return a page'
+    exit
+  end
+
+  parsed_page = Nokogiri.HTML(unparsed_page)
+  
+  job_listings = parsed_page.css('div.jobsearch-SerpJobCard')
+  puts per_page = job_listings.count
+  
+  puts total =
+         parsed_page.css(
+           'div#searchCountPages'
+         ).text.strip.split[
+           3
+         ].delete(',')
+         
+  return (total.to_i/per_page) + 1
+end
+
+def scraper(page)
+  curUrl = URL + page.to_s
+  unparsed_page = HTTParty.get(curUrl)
+  
   if unparsed_page.body.nil? || unparsed_page.body.empty?
     puts 'GET did not return a page'
     exit
@@ -13,25 +40,23 @@ def scraper
   parsed_page = Nokogiri.HTML(unparsed_page)
   jobs = Array.new
   job_listings = parsed_page.css('div.jobsearch-SerpJobCard')
-  per_page = job_listings.count
-  puts total =
-         parsed_page.css(
-           'div#searchCountPages'
-         ).text.strip.split[
-           3
-         ]
+  
   job_listings.each do |job_listing|
     job = {
-      title: job_listing.css('a.jobtitle').text,
-      company: job_listing.css('span.comapany'),
-      location: job_listing.css('div.location').text,
-      url:
-        'https://www.indeed.co.uk' +
-          job_listing.css('a')[0].attributes['href'].value
+      title: job_listing.css('a.jobtitle').text.gsub("\n",""),
+      company: job_listing.css('span.company').text.gsub("\n",""),
+      location: job_listing.css('div.location').text.gsub("\n",""),
+      # url:
+      #   'https://www.indeed.co.uk' +
+      #     job_listing.css('a')[0].attributes['href'].value
     }
     jobs << job
   end
-  byebug
+  puts jobs.to_s
 end
 
-scraper
+# (1..getTotal).each { |num|
+#    scraper(num*10)
+# } 
+
+puts getTotal
